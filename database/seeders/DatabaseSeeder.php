6de18553da7f3c03454bbeeae85144abd75ac64c
BilalElngar <blalsame123@gmail.com>
2026-07-25 03:32:11 +0300

    Add ThirtyKTeachersSeeder - clear and recreate 30k teachers competition data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            DemoDataSeeder::class,
+            ThirtyKTeachersSeeder::class,
         ]);
     }
 }
