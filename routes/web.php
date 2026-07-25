00258a00ea82ddb10afa2aed9bd36dcaba0e37b0
BilalElngar <blalsame123@gmail.com>
2026-07-25 14:03:21 +0300

    Show welcome page at / instead of bare OK

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 // المسار الرئيسي الذي تفحصه المنصة دائماً (يجب أن يعيد 200 OK مباشرة)
 Route::get('/', function () {
-    // إذا كان الطلب من منصة الفحص أو بدون طلب Inertia معقد، أرجع نص أو استجابة صريحة
-    if (request()->header('X-Inertia') === null && !request()->expectsJson()) {
-        return response('OK', 200);
-    }
-
-    // غير ذلك اعرض صفحة الترحيب العادية
     return inertia('welcome');
 })->name('home');
 
@@ -22,7 +16,6 @@ Route::prefix('guide')->group(function () {
     Route::inertia('faq', 'guide/faq')->name('guide.faq');
     Route::inertia('resources', 'guide/resources')->name('guide.resources');
 });
-
 Route::inertia('faq', 'faq')->name('faq');
 Route::inertia('resources', 'resources')->name('resources');
 
